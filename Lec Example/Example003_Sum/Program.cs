df91bda46c626a8c4376568904064cdedb5654d3
Карина Миронова <kamironova@192.168.1.4>
2023-05-23 14:18:55 +0300

    example003 sum

---
+++ Lec Example/Example003_Sum/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberA = 3;
+int numberB = 5;
+System.Console.WriteLine(numberA + numberB);
+
+
+// Второй ариант
+
+// int numberA = 5;
+// int numberB = 4;
+// int result = numberA + numberB;
+// System.Console.WriteLine(result);
