e82dde7538aa5f249aa2a6148d902866c714736c
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-05-24 12:36:34 +0300

    If_Else

---
+++ Lec Example/Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Enter your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    System.Console.WriteLine("Hello, super Masha!!!");
+}
+
+else  {
+    System.Console.Write("Hi, ");
+    System.Console.WriteLine(username);
+}
