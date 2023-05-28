be1aa2b695162bfb99ebc63a9e78a3f2e320b90a
Карина Миронова <kamironova@192.168.1.4>
2023-05-28 20:57:04 +0300

    Seminar

---
+++ Lec Example/Seminar_C#/Example001/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу которая на вход принимает число и выдает его квадрат. 
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+System.Console.WriteLine("Введите целое число -> ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num * num;
+System.Console.WriteLine("Квадрат числа --> " + result);
+
+// Дополнительный оператор ветвления
+
+if (num > 0)
+{
+    System.Console.WriteLine("Вы ввели положительное число");
+
+}
+else if (num == 0)
+    System.Console.WriteLine("Вы ввели Ноль");
+else
+    System.Console.WriteLine("Вы ввели отрицательное число ");
