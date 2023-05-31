abddbfd203fc1d7d3e888548dff9e1eb3c9d5e40
Карина Миронова <kamironova@192.168.1.4>
2023-05-31 12:57:51 +0300

    ex6

---
+++ Lec Example/Seminar_C#/Example006/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написоть программу, которая принимает на вход число 
+// любой разрядности и выдает его третью цифру.
+// Например:
+// 123456787 --> 3:
+// 5678 --> 7;
+
+Console.Clear();
+
+System.Console.WriteLine("ВВедите любое число --> ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100)
+{
+    while (num / 1000 >0)
+    {
+        num /= 10;
+    }
+    int d = num %10;
+    System.Console.WriteLine(d);
+}
+else System.Console.WriteLine("Третьей цифры нет");
