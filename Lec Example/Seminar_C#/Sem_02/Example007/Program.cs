5340a6d0c96fec487f2e42440a2f7f90116a90db
Карина Миронова <kamironova@192.168.1.3>
2023-06-06 14:55:41 +0300

    sem 02 Ex 01

---
+++ Lec Example/Seminar_C#/Sem_02/Example007/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру числа
+// Напимер:
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+Console.Clear();
+
+int num = new Random().Next(10, 100);
+System.Console.WriteLine($"Сгениерированное число {num}");
+
+int x1 = num / 10;
+int x2 = num % 10;
+
+if (x1 > x2)
+{
+    System.Console.WriteLine($"{num} -> {x1}");
+}
+else if (x1 < x2)
+{
+    System.Console.WriteLine($"{num} -> {x2}");
+}
+else
+{
+    System.Console.WriteLine("Обе цифры равны");
+}
