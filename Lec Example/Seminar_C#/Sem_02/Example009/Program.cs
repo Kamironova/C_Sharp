6b69c5f8c9579475ce48aa41591fef319edf25b2
Карина Миронова <kamironova@192.168.1.3>
2023-06-06 16:03:17 +0300

    Ex 09

---
+++ Lec Example/Seminar_C#/Sem_02/Example009/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 12. Напишите программу, которая будет принимать на вход два числа
+// и выводить, является ли вроторое число кратным первому. 
+// Если число 2 не кратно числу 1, то выводить остаток от деления
+// 34, 5 --> не кратно, остаток 4
+// 16, 4 --> кратно;
+
+Console.Clear();
+
+System.Console.WriteLine("Введите первое число --> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число -->");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a % b == 0)
+{
+    System.Console.WriteLine($"{a},{b} --> кратно");
+}
+else 
+{
+    int x = a % b;
+    System.Console.WriteLine($"{a}, {b} --> не кратно, остаток от деления {x}");
+}
