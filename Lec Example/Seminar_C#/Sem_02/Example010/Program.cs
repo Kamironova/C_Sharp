b1751407b80706dbe08e0fa39a07197fc8175a15
Карина Миронова <kamironova@192.168.1.3>
2023-06-07 15:56:00 +0300

    ex010

---
+++ Lec Example/Seminar_C#/Sem_02/Example010/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 14. Напишите программу, которая принимает на вход число 
+// и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 --> нет
+// 46 --> нет
+// 161 --> да
+
+Console.Clear();
+
+System.Console.WriteLine("Введите число --> ");
+int x = int.Parse(Console.ReadLine()!);
+
+if (x % 7 == 0 && x % 23 == 0)
+{
+    System.Console.WriteLine($"{x} --> да");
+}
+else
+{
+    System.Console.WriteLine($"{x} --> нет");
+}
