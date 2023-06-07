04a91b0a16b56f75b3e81180461f8826ef2b1a5f
Карина Миронова <kamironova@192.168.1.3>
2023-06-07 16:12:33 +0300

    ex 11

---
+++ Lec Example/Seminar_C#/Sem_02/Example011/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 16. Напишите программу, которая принимает на вход два
+//  числа и проверяет является ли одно квадратом другого
+// 5, 25 --> да
+// -4, 16 --> да
+// 25,5 --> да
+// 8, 9 --> нет
+
+Console.Clear();
+
+System.Console.WriteLine("Введите первое число --> ");
+int x1 = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите второе число --> ");
+int x2 = int.Parse(Console.ReadLine()!);
+
+if (x1 % x2 == 0 || x2 % x1 == 0)
+{
+    System.Console.WriteLine($"{x1}, {x2} --> да");
+}
+else
+{
+    System.Console.WriteLine($"{x1}, {x2} --> нет");
+}
