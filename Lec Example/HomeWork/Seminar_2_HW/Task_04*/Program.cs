e121f02aa484dc0cc38b320612ee855ca30a475d
Карина Миронова <kamironova@192.168.1.3>
2023-06-09 13:41:03 +0300

    HW

---
+++ Lec Example/HomeWork/Seminar_2_HW/Task_04*/Program.cs
@@ -0,0 +1,18 @@
+﻿// ДОП ЗАДАНИЕ. На входе целое число любой разрядности
+// надо удалить вторую цифру (слева на право) этого числа
+
+Console.Clear();
+
+System.Console.WriteLine("Введите число любой разрядности --> ");
+int x = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+while (x >= 10)
+{
+    x = x/10;
+    count++;
+}
+
+System.Console.WriteLine(count);
+
+// ДОДЕЛАТЬ!!
