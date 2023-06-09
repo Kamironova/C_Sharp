8d6a640dc5179922076dd866016c52072de46362
Карина Миронова <kamironova@192.168.1.3>
2023-06-09 16:08:51 +0300

    Exa 12

---
+++ Lec Example/1_Lesson_003/Cycle_in_Cycle/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выводим таблицу умножения на экран при помощи цикла в цикле
+
+Console.Clear();
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 0; j <= 10; j++)
+    {
+        System.Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    System.Console.WriteLine();
+}
