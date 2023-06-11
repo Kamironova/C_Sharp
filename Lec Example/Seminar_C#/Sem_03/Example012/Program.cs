33d95b03d14bf60788c1faa0ee95483768ed024f
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-06-11 15:03:36 +0300

    111

--- Lec Example/Seminar_C#/Sem_03/Example012/Program.cs
+++ Lec Example/Seminar_C#/Sem_03/Example012/Program.cs
@@ -2,7 +2,7 @@
 // причем х не равно 0 и y не равно 0 и выдает номер четверти плоскости 
 // в которой находится эта точка
 
-void ChecKoord(int x, int y)
+void CheckKoord(int x, int y)
 {
     if (x > 0 && y > 0) System.Console.WriteLine(" 1 quarter");
     else if (x < 0 && y > 0) System.Console.WriteLine(" 2 quarter");
@@ -10,6 +10,25 @@ void ChecKoord(int x, int y)
     else if (x > 0 && y < 0) System.Console.WriteLine(" 4 quarter");
     else System.Console.WriteLine("Точка находится на координатной оси");
 }
+
+// Или функция, которая возвращает:
+
+int CheckKoord2(int x, int y) // возвращает значение
+{
+    int result = 0;
+    if (x > 0 && y > 0)
+        result = 1;
+    else if (x < 0 && y > 0)
+        result = 2;
+    else if (x < 0 && y < 0)
+        result = 3;
+    else if (x > 0 && y < 0)
+        result = 4;
+    return result;
+}
+
+
+
 
 Console.Clear();
 
@@ -19,7 +38,8 @@ int x = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("y --> ");
 int y = Convert.ToInt32(Console.ReadLine());
 
-ChecKoord(x,y);
+//CheckKoord(x, y);
+System.Console.WriteLine($"Это четверть №{CheckKoord2(x,y)}");
 
 // if (x > 0 && y > 0) System.Console.WriteLine(" 1 quarter");
 // else if (x < 0 && y > 0) System.Console.WriteLine(" 2 quarter");
