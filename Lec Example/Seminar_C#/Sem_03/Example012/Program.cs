cf5816c6a926dd8343a89b4e874a701aa42020c7
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-06-11 14:32:08 +0300

    x-y

---
+++ Lec Example/Seminar_C#/Sem_03/Example012/Program.cs
@@ -0,0 +1,17 @@
+﻿//напишите программу, которая принимает на вход координаты точки (x,y),  
+// причем х не равно 0 и y не равно 0 и выдает номер четверти плоскости 
+// в которой находится эта точка
+
+Console.Clear();
+
+System.Console.Write("x --> ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("y --> ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0) System.Console.WriteLine(" 1 quarter");
+else if (x < 0 && y > 0) System.Console.WriteLine(" 2 quarter");
+else if (x < 0 && y < 0) System.Console.WriteLine(" 3 quarter");
+else if (x > 0 && y < 0) System.Console.WriteLine(" 4 quarter");
+else System.Console.WriteLine("Точка находится на координатной оси");
