508badbf42358fc9856e7631c719030b7d3c8191
Карина Миронова <kamironova@192.168.1.3>
2023-06-13 17:07:48 +0300

    111

--- Lec Example/Seminar_C#/Sem_03/Example013/Program.cs
+++ Lec Example/Seminar_C#/Sem_03/Example013/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Задача 18. Напишите программу, которая по заданному номеру четверти,
 // показывавет диапазо возможных координат точек в этой чветрверти(x,y)
+
+
 void Koord(int q)
 {
     if (q == 1) System.Console.WriteLine($"{q} находится в диапазоне: x > 0 && y > 0");
@@ -8,8 +10,6 @@ void Koord(int q)
     else if (q == 4) System.Console.WriteLine($"{q} Находится в диапазоне: x > 0 && y < 0 ");
     else if (q > 4) System.Console.WriteLine("Введите число от 1 до 4");
 };
-
-
 
 
 Console.Clear();
@@ -39,10 +39,3 @@ Koord(q);
 // {
 //     System.Console.WriteLine("Введите число от 1 до 4");
 // }
-
-
-// if (x > 0 && y > 0) System.Console.WriteLine(" 1 quarter");
-// else if (x < 0 && y > 0) System.Console.WriteLine(" 2 quarter");
-// else if (x < 0 && y < 0) System.Console.WriteLine(" 3 quarter");
-// else if (x > 0 && y < 0) System.Console.WriteLine(" 4 quarter");
-// else System.Console.WriteLine("Точка находится на координатной оси")
