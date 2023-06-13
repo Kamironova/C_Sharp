3c004e78b227eb1a86c12feccfba20a48d272007
Карина Миронова <kamironova@192.168.1.3>
2023-06-13 17:21:07 +0300

    Koord

--- Lec Example/Seminar_C#/Sem_03/Example013/Program.cs
+++ Lec Example/Seminar_C#/Sem_03/Example013/Program.cs
@@ -2,14 +2,26 @@
 // показывавет диапазо возможных координат точек в этой чветрверти(x,y)
 
 
-void Koord(int q)
+// void Koord(int q)
+// {
+//     if (q == 1) result = "x > 0 && y > 0");
+//     else if (q == 2) result = "x < 0 && y > 0");
+//     else if (q == 3) result = "x < 0 && y < 0");
+//     else if (q == 4) result = "x > 0 && y < 0 ");
+//     else if (q > 4) System.Console.WriteLine("Введите число от 1 до 4");
+// };
+
+
+string Koord2(int q)
 {
-    if (q == 1) System.Console.WriteLine($"{q} находится в диапазоне: x > 0 && y > 0");
-    else if (q == 2) System.Console.WriteLine($"{q} находится в диапазоне: x < 0 && y > 0");
-    else if (q == 3) System.Console.WriteLine($"{q} находится в диапазоне: x < 0 && y < 0");
-    else if (q == 4) System.Console.WriteLine($"{q} Находится в диапазоне: x > 0 && y < 0 ");
-    else if (q > 4) System.Console.WriteLine("Введите число от 1 до 4");
-};
+    string result = string.Empty;
+    if (q == 1) result = "x > 0 && y > 0";
+    else if (q == 2) result = "x < 0 && y > 0";
+    else if (q == 3) result = "x < 0 && y < 0";
+    else if (q == 4) result = "x > 0 && y < 0 ";
+    else if (q > 4) result = "Введите число от 1 до 4";
+    return result;
+}
 
 
 Console.Clear();
@@ -17,23 +29,27 @@ Console.Clear();
 System.Console.WriteLine("Введите номер четрвети --> ");
 int q = Convert.ToInt32(Console.ReadLine());
 
-Koord(q);
+//Koord(q);
+Koord2(q);
+System.Console.WriteLine($"{Koord2(q)}");
+
+
 
 // if (q == 1)
 // {  
-//     System.Console.WriteLine($"{q} находится в диапазоне: x > 0 && y > 0");
+//     result = "x > 0 && y > 0");
 // }
 // else if (q == 2)
 // {
-//     System.Console.WriteLine($"{q} находится в диапазоне: x < 0 && y > 0");
+//     result = "x < 0 && y > 0");
 // }
 // else if (q == 3)
 // {
-//     System.Console.WriteLine($"{q} находится в диапазоне: x < 0 && y < 0");
+//     result = "x < 0 && y < 0");
 // }
 // else if (q == 4)
 // {
-//     System.Console.WriteLine($"{q} Находится в диапазоне: x > 0 && y < 0 ");
+//     result = "x > 0 && y < 0 ");
 // }
 // else if (q >4)
 // {
