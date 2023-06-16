214f8da86f3b1e58821b87949a5ae44afd05b12a
Карина Миронова <kamironova@192.168.1.3>
2023-06-16 14:23:26 +0300

    111

---
+++ Lec Example/Seminar_C#/Sem_03/Example_014/Program.cs
@@ -0,0 +1,13 @@
+﻿// Звдача 21. Напишите программу, которая принимае на вход координаты
+// двух точек в 2D пространстве.
+// Например:
+// A(3,6); B(2,1) --> 5,09
+// A(7,5); B(1,-1) --> 7,21
+
+Console.Clear;
+
+System.Console.WriteLine("Введите число 1 --> ");
+int a = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите число 2 --> ");
+int b = int.Parse(Console.ReadLine()!);
