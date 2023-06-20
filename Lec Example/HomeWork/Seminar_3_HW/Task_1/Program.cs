b1b35b27589539a5444f97c2533dc4af6c275f5a
Карина Миронова <kamironova@192.168.1.3>
2023-06-20 14:00:16 +0300

    hoe

---
+++ Lec Example/HomeWork/Seminar_3_HW/Task_1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход 
+//пятизначное число и проверяет, является ли оно палиндромом.
+//Через строку решать нельзя.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// void Pall(int[] array)
+// {
+//     if (array[0] == array[4] && array[1] == array[3])
+//     {
+//         System.Console.WriteLine($"Число является палиндромом");
+//     }
+//     else
+//     {
+//         System.Console.WriteLine($"число не является палиндромом");
+//     }
+// }
+
+
+// НЕ ПОЛУЧАЕТСЯ РЕШИТЬ НЕ ПОНИМАЮ ЧТО НЕ ТАК
+
+
+Console.Clear();
+
+System.Console.WriteLine("Введите пятизначное число --> ");
+int x = Convert.ToInt32(Console.ReadLine()!);
+
+int[] array = { x };
+
+//int index = array.Length;
+if (array[0] == array[3] && array[1] == array[3])
+{
+    System.Console.WriteLine($"Число является палиндромом");
+}
+else
+{
+    System.Console.WriteLine($"число не является палиндромом");
+}
+
+//Pall(array);
+
+
+
