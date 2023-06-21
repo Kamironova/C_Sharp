214537f8b869f45376bf3f8e286e9020d8dd0fb6
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-06-21 11:42:06 +0300

    sem

---
+++ Lec Example/Seminar_C#/Sem_04/Example016/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 24. Напишите программу, которая принимает
+// на вход число (А) и выдает сумму сисел от 1 до А.
+// 7 --> 28
+// 4 --> 10
+// 8 --> 36
+
+// !!! далее 2 варианта решения через фуекцию void и через возвращающую
+
+int Sum(int a)
+{
+    int result = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        result+=i;  // result = result + i
+    }
+    return result;
+}
+
+void Sum2(int a)
+{
+    int result = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        result += i;
+    }
+    System.Console.WriteLine($"{a} --> {result}");
+}
+
+ 
+Console.Clear();
+
+System.Console.WriteLine("Enter --> ");
+int a = int.Parse(Console.ReadLine()!);
+
+//System.Console.WriteLine($"{a} --> {Sum(a)}");
+Sum2(a);
