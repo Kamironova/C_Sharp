191ae304682299b15826de1167a6a9b0edc7a16c
Карина Миронова <kamironova@192.168.1.3>
2023-06-21 16:59:09 +0300

    fact+array

--- Lec Example/Seminar_C#/Sem_04/Example019/Program.cs
+++ Lec Example/Seminar_C#/Sem_04/Example019/Program.cs
@@ -4,34 +4,55 @@
 
 
 
-void FillArray(int[] collection)     // метод для генерации случайных чисел
-{
-    int length = collection.Length;
-    int index = 0;
-    while (index < length)
-    {
-        collection[index] = new Random().Next(0, 2);
-        index++;
-    }
-}
+// void FillArray(int[] collection)     // метод для генерации случайных чисел
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index] = new Random().Next(0, 2);
+//         index++;
+//     }
+// }
 
-void PrintArray(int[] col)    // метод для печати сгенерированных случайных чисел
+// void PrintArray(int[] col)    // метод для печати сгенерированных случайных чисел
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         Console.Write($"{col[position]},");
+//         position++;
+//     }
+// }
+
+
+
+ // Console.Clear();
+
+// int[] array = new int[8];
+
+// FillArray(array);
+// PrintArray(array);
+
+
+// Второй вариант:
+
+int[] FillArray1()
 {
-    int count = col.Length;
-    int position = 0;
-    while (position < count)
+    int[] array = new int[8];
+    Random random = new Random();
+    for (int i = 0; i < 8; i++)
     {
-        Console.Write($"{col[position]},");
-        position++;
+        array[i] = random.Next(2);
     }
+    return array;
 }
 
 
 
 Console.Clear();
 
-int[] array = new int[8];
-
-FillArray(array);
-PrintArray(array);
+int[] array = FillArray1();
+System.Console.WriteLine(string.Join(",", array));
 
