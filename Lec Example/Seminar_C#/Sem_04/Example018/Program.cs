113d594b4a04d3cc2caffcef67ddb46607dede33
Карина Миронова <kamironova@192.168.1.3>
2023-06-21 16:39:50 +0300

    fact+array

---
+++ Lec Example/Seminar_C#/Sem_04/Example018/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28. Напишите программу, которая принимает на вход число N
+// и выдает произведение числе от 1 до N ( факториал )
+// 4 --> 24
+// 5 --> 120
+
+int Fact(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result = i * result;
+    }
+    return result;
+}
+
+
+
+
+Console.Clear();
+
+System.Console.WriteLine("Enter --> ");
+int n = Convert.ToInt32(Console.ReadLine()!);
+
+System.Console.WriteLine($"{n} --> {Fact(n)}");
