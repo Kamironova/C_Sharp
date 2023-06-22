e57c1239127f0971bafe3715f0a498cf815ad3b3
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-06-22 12:56:12 +0300

    home 1

--- Lec Example/HomeWork/Seminar_4_HW/Task_01/Program.cs
+++ Lec Example/HomeWork/Seminar_4_HW/Task_01/Program.cs
@@ -3,3 +3,49 @@
 // Math.Pow юзать нельзя.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+
+// DONE !!!!!!
+
+// Вариант с циклом while
+
+// int Pow(int a, int b)
+// {
+//     int i = 1;
+//     int result = 1;
+
+//     while (i <= b)
+//     {
+//         result = result * a;
+//         i++;
+//     }
+//     return result;
+
+// }
+
+
+// Вариант с циклом for
+
+int Pow2(int a, int b)
+{
+    int result = 1;
+
+    for (int i = 1; i <= b; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+
+
+Console.Clear();
+
+System.Console.WriteLine("Введите число A --> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число B --> ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+System.Console.WriteLine($"{a} в степени {b} --> {Pow2(a,b)}");
+
