a94e536b5eebeb8b380a559afc779c4f4f7f854e
Карина Миронова <kamironova@192.168.1.3>
2023-08-21 15:51:06 +0300

    HW 2

---
+++ Lec Example/Seminar__/Task01/Program.cs
@@ -0,0 +1,65 @@
+﻿// Напишите программу, которая на вход принимает два числа 
+// и проверяет, является ли первое квадратом второго
+//Например:
+// a=25, b=5 -> Да
+// a=2, b = 10 -> Нет
+// 9, -3 -> Да
+// -3, 9 -> Нет
+
+Console.Clear();
+
+System.Console.WriteLine("Введите первое число --> ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число --> ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 == number1 / number2)
+{
+    System.Console.WriteLine($"{number1} является квадратом числа{number2}");
+}
+else
+{
+    System.Console.WriteLine($"{number1} не является квадратом числа {number2}");
+}
+
+
+//Или
+
+
+// Console.Clear();
+
+// System.Console.WriteLine("Enter First Number -> ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine("Enter Second Number -> ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a == (b*b))
+// {
+//     System.Console.WriteLine($"Ура {a} квадрат {b}");
+// }
+// else 
+// {
+//     System.Console.WriteLine("Не квадрат");
+// }
+
+
+// Третий вариант вариант
+
+// Console.WriteLine("Enter first nuber: ");
+// string? str = Console.ReadLine();
+// int firstNumber = int.Parse(str);
+
+// Console.WriteLine("Enter second nuber: ");
+// str = Console.ReadLine();
+// int secondNumber = int.Parse(str);
+
+// if ((Math.Pow(secondNumber, 2)) == firstNumber)  // эта команда возводит в квадрат
+// {
+//     Console.WriteLine("Второе число является квадратом первого");
+// }
+// else
+// { 
+//     Console.WriteLine("Второе число не является квадратом первого");
+//}
---
+++ Lec Example/Seminar__/Task03/Program.cs
@@ -0,0 +1,94 @@
+﻿// Напишите программу, которая будет выдавать название дня
+// недели по заданному номеру
+// Например:
+// 3. -> Среда;
+// 5. -> Пятница.
+
+
+Console.Clear();
+
+System.Console.WriteLine("Введите число --> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    System.Console.WriteLine("Понедельник");
+}
+else if (number == 2)
+{
+    System.Console.WriteLine("Вторник");
+}
+else if (number == 3)
+{
+    System.Console.WriteLine("Среда");
+}
+else if (number == 4)
+{
+    System.Console.WriteLine("Четверг");
+}
+else if (number == 5)
+{
+    System.Console.WriteLine("Пятница");
+}
+else if (number == 6)
+{
+    System.Console.WriteLine("Суббота");
+}
+else if (number == 7)
+{
+    System.Console.WriteLine("Воскресенье");
+}
+else
+{
+    System.Console.WriteLine("Введите число от 1 до 7");
+}
+
+
+// Второй вариант
+
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// switch (a)  
+// {
+//     case 1:
+//     Console.WriteLine("Понедельник");
+//     break;
+
+//     case 2:
+//     Console.WriteLine("Вторник");
+//     break;
+
+//     case 3:
+//     Console.WriteLine("Среда");
+//     break;
+
+//     case 4:
+//     Console.WriteLine("Четверг");
+//     break;
+
+//     case 5:
+//     Console.WriteLine("Пятница");
+//     break;
+
+//     case 6:
+//     Console.WriteLine("Суббота");
+//     break;
+
+//     case 7:
+//     Console.WriteLine("Воскресенье");
+//     break;
+
+//     default :
+//     Console.WriteLine("Введите число 1-7");
+//     break;
+// }
+
+
+// Третий вариант:
+
+// System.Console.WriteLine("--> ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+
+// System.Console.WriteLine((DayOfWeek)a);
