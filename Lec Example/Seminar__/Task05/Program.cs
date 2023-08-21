79bc438ed3accb34dfd9710b48d5d10f946bb328
Карина Миронова <kamironova@192.168.1.3>
2023-08-21 17:03:11 +0300

    HW 2

---
+++ Lec Example/Seminar__/Task05/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напищите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа а промежутке от -N до N.
+// Например:
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+
+Console.Clear();
+
+System.Console.WriteLine("Введите число --> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = number * -1; i <= number; i++)  // можно -number вместо number * -1
+{
+    System.Console.Write($"{i} ");
+}
+System.Console.WriteLine();
