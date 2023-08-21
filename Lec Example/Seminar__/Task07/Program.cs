6b8b49b1716624ca8fade3a80a0c6fc9977115a6
Карина Миронова <kamironova@192.168.1.3>
2023-08-21 17:22:00 +0300

    task

---
+++ Lec Example/Seminar__/Task07/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Clear();
+
+System.Console.WriteLine("Введите трехзначное число -->");
+int num = Convert.ToInt32(Console.ReadLine());
+
+//int num2 = num %10;
+//System.Console.WriteLine(num2);
+
+if (num >99 && num <= 999 )
+{
+System.Console.WriteLine(num % 10);
+}
+else 
+{
+    System.Console.WriteLine("Ведите трехзначное число!");
+}
