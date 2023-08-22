854427fded4a0d284be85f102e45ba658b30d71f
Карина Миронова <kamironova@192.168.1.3>
2023-08-22 20:49:15 +0300

    task hw

--- Lec Example/HomeWork/Seminar_4_HW/Task_03/Program.cs
+++ Lec Example/HomeWork/Seminar_4_HW/Task_03/Program.cs
@@ -7,35 +7,35 @@
 
 
 
-int ArrPrint(int[] array)
-{
-    int[] length = new int[8];
-    //int i = 0;
-    for (int i = 0; i <= 8; i++)
-    {
-        array[i] = new Random().Next(0, 100);
-        System.Console.Write($"{array[i]} ");
-    }
-    return array[1];
-}
+// int ArrPrint(int[] array)
+// {
+//     int[] length = new int[8];
+//     //int i = 0;
+//     for (int i = 0; i <= 8; i++)
+//     {
+//         array[i] = new Random().Next(0, 100);
+//         System.Console.Write($"{array[i]} ");
+//     }
+//     return array[1];
+// }
 
-void PrintArray(int[] col)    // метод для печати сгенерированных случайных чисел
-{
-    int count = col.Length;
-    int position = 0;
-    while (position < count)
-    {
-        Console.Write($"{col[position]},");
-        position++;
-    }
-}
+// void PrintArray(int[] col)    // метод для печати сгенерированных случайных чисел
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         Console.Write($"{col[position]},");
+//         position++;
+//     }
+// }
 
 
 Console.Clear();
 
-int[] array = new int[8];
-ArrPrint(array);
-PrintArray(array);
+// int[] array = new int[8];
+// ArrPrint(array);
+// PrintArray(array);
 
 
 
@@ -72,7 +72,7 @@ void PrintArray(int[] array)   // Функция печатает сгенери
 
 Console.Clear();
 
-int[] array = new int[12];
+int[] array = new int[8];
 FillArray(array);
 PrintArray(array);
 
