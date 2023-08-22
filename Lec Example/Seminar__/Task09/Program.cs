ac38fc0c649c8b0d45c1dac8f1418fb1b6ace957
Карина Миронова <kamironova@192.168.1.3>
2023-08-22 17:43:29 +0300

    task

---
+++ Lec Example/Seminar__/Task09/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит случайное число
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+// Валерия Шарандина 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+Console.Clear();
+
+int number = new Random().Next(10, 100);
+
+int numA = number / 10;
+int numB = number % 10;
+
+if (numA > numB)
+{
+    System.Console.WriteLine($"{number} --> max {numA}");
+}
+else
+{
+    System.Console.WriteLine($"{number} --> max {numB}");
+}
---
+++ Lec Example/Seminar__/Task11/Program.cs
@@ -0,0 +1,17 @@
+﻿// 11. Напишите программу, которая выводит случайное трёхзначное 
+// число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Console.Clear();
+
+int number = new Random().Next(100, 1000);
+
+int numA = number / 100;
+int numB = number % 10;
+int result = (numA * 10) + numB;
+
+System.Console.WriteLine($"{number} --> {result}");
+
+//System.Console.WriteLine($"{number} --> {numA}{numB}");
