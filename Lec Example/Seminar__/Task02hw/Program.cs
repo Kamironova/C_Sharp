97c7c2148c42a62d6e0f45a85558ab5f65477282
Карина Миронова <kamironova@192.168.1.3>
2023-08-22 19:59:27 +0300

    task hw

---
+++ Lec Example/Seminar__/Task02hw/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+
+System.Console.WriteLine("Введите два числа -->");
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB)
+{
+    System.Console.WriteLine($"max --> {numA}");
+}
+else
+{
+    System.Console.WriteLine($"max --> {numB}");
+}
---
+++ Lec Example/Seminar__/Task06hw/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Clear();
+
+System.Console.Write("--> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 ==0)
+{
+    System.Console.WriteLine("Четное");
+}
+else
+{
+    System.Console.WriteLine("Не четное");
+}
