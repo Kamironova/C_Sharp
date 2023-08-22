e17ba9c1d0cecb74d04bade2d9f1c4178249e324
Карина Миронова <kamironova@192.168.1.3>
2023-08-22 16:18:28 +0300

    task

---
+++ Lec Example/Seminar__/Task12/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 12. Напишите программу, которая будет принимать на вход два числа
+// и выводить, является ли вроторое число кратным первому. 
+// Если число 2 не кратно числу 1, то выводить остаток от деления
+// 34, 5 --> не кратно, остаток 4
+// 16, 4 --> кратно;
+
+Console.Clear();
+
+System.Console.WriteLine("Введите два числа --> ");
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int result = numA % numB;
+
+if (result == 0)
+{
+    System.Console.WriteLine($"Число {numB} кратно {numA} ");
+}
+
+else 
+{
+    System.Console.WriteLine($"Числа {numB} и {numA} не кратны, процент от остатка {result}");
+}
---
+++ Lec Example/Seminar__/Task16/Program.cs
@@ -0,0 +1,21 @@
+﻿// 16. Напишите программу, которая принимает на вход два числа
+// и проверяет, является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.Clear();
+
+System.Console.WriteLine("Введоие два числа --> ");
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA % numB == 0 || numB % numA == 0)
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
