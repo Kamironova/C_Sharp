55a58443d8a514730405a14408caa288a9b5ee8e
Карина Миронова <kamironova@192.168.1.3>
2023-08-22 10:40:28 +0300

    task18

---
+++ Lec Example/Seminar__/Task17/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (x,y),  
+// причем х не равно 0 и y не равно 0 и выдает номер четверти плоскости 
+// в которой находится эта точка
+
+Console.Clear();
+
+System.Console.WriteLine("Введите координаты X и Y -->");
+int userX = Convert.ToInt32(Console.ReadLine());
+int userY = Convert.ToInt32(Console.ReadLine());
+
+
+if (userX > 0 && userY > 0)
+{
+    System.Console.WriteLine($"{userX} и {userY} находятся в 1 четверти");
+}
+
+else if (userX < 0 && userY > 0)
+{
+    System.Console.WriteLine($"{userX} и {userY} находятся в 2 четверти");
+}
+
+else if (userX < 0 && userY < 0)
+{
+    System.Console.WriteLine($"{userX} и {userY} находятся в 3 четверти");
+}
+else if (userX > 0 && userY < 0)
+{
+    System.Console.WriteLine($"{userX} и {userY} находятся в 4 четверти");
+}
---
+++ Lec Example/Seminar__/Task18/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 18. Напишите программу, которая по заданному номеру четверти,
+// показывавет диапазо возможных координат точек в этой чветрверти(x,y)
+
+
+Console.Clear();
+
+System.Console.WriteLine("Введите номер четрвети --> ");
+int q = Convert.ToInt32(Console.ReadLine());
+if (q == 1) Console.WriteLine("x > 0 && y > 0");
+else if (q == 2) Console.WriteLine("x < 0 && y > 0");
+else if (q == 3) Console.WriteLine("x < 0 && y < 0");
+else if (q == 4) Console.WriteLine("x > 0 && y < 0 ");
+else if (q > 4) System.Console.WriteLine("Введите число от 1 до 4");
