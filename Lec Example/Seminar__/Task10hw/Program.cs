1ded1ad095458d39d661eed38090aa5a5a52a94d
Карина Миронова <kamironova@192.168.1.3>
2023-08-22 20:42:22 +0300

    task hw

---
+++ Lec Example/Seminar__/Task10hw/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+
+System.Console.WriteLine("Введите трехзначное число --> ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 100 && number < 1000)
+{
+    int numA = number % 100 / 10;
+    System.Console.WriteLine(numA);
+}
+else
+{
+    System.Console.WriteLine("Ошибка, введите трехзнвачное число");
+}
---
+++ Lec Example/Seminar__/Task15hw/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+
+System.Console.Write("--> ");
+int week = Convert.ToInt32(Console.ReadLine());
+
+if (week <8 && week >=1)
+{
+    if (week == 6 || week == 7)
+    {
+        System.Console.WriteLine("Ура, выходной!");
+    }
+    else
+    {
+        System.Console.WriteLine("Будний день");
+
+    }
+}
+else
+{
+    System.Console.WriteLine("Ошибка, введите число от 1 до 7");
+}
