23e63b8a5dd4e4dea70741afd059278bd7987f06
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-08-26 18:09:24 +0300

    hw25

---
+++ Lec Example/Seminar__/Task25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Product(int numberA, int numberB)
+{
+    int result = 1;
+
+    for (int count = 1; count <= numberB; count++)
+    {
+        result = result * numberA;
+    }
+    return result;
+}
+
+
+Console.Clear();
+
+System.Console.WriteLine("Введите два числа --> ");
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(Product(numA, numB));
