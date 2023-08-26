123b271e1b5177caf14d5386b742bf2d8393f761
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-08-26 18:36:08 +0300

    hw27

---
+++ Lec Example/Seminar__/Task27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int NumberSum(int number)
+{
+    int sum = 0;
+    int num = 0;
+
+    while (number > 0)
+    {
+        num = number % 10;
+        sum = sum + num;
+        number = number/ 10;
+    }
+    return sum;
+}
+
+Console.Clear();
+
+System.Console.WriteLine("--> ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+
+
+System.Console.WriteLine($"Сумма цифр числа {userNumber} --> {NumberSum(userNumber)}");
