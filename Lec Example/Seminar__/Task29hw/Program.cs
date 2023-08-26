f326e8432179b1beccd8ee64b4f3b41a9aea9675
Карина Миронова <kamironova@192.168.1.2>
2023-08-26 23:04:54 +0300

    hw29

---
+++ Lec Example/Seminar__/Task29hw/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+int[] FillArray(int collection)
+{
+    int[] array = new int[collection];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99, 100);
+    }
+    return array;
+}
+
+
+
+void PrintArray(int[] arrayToPrint)
+{
+    System.Console.Write("[ ");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        System.Console.Write($"{arrayToPrint[i]} ");
+    }
+    System.Console.Write("]");
+    System.Console.WriteLine();
+}
+
+Console.Clear();
+
+System.Console.Write("Введите количество элементов массива -->");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int[] array = FillArray(userNumber);
+PrintArray(array);
