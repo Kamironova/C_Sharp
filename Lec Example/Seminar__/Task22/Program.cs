a9de0c4e2ff78a769f1a5058e324c4555a052ab9
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-08-28 12:03:38 +0300

    hw22

--- Lec Example/Seminar__/Task22/Program.cs
+++ Lec Example/Seminar__/Task22/Program.cs
@@ -1,4 +1,20 @@
-﻿Console.Clear();
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу квадратов от 1 до N
 
-System.Console.WriteLine("Введите число --> ");
+
+Console.Clear();
+
+System.Console.Write("Введите число --> ");
 int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0)
+{
+    num = num * (-1);
+}
+
+for (int i = 1; i <= num; i++)
+{
+    int result = i * i;
+    System.Console.Write($" {result}");
+}
+System.Console.WriteLine();
