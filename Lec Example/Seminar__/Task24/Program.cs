f15308c33269719d8f38e651f226fd717f5d5164
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-08-28 12:16:41 +0300

    hw24

---
+++ Lec Example/Seminar__/Task24/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А.
+
+Console.Clear();
+
+System.Console.WriteLine("--> ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+
+if (userNumber<0)
+{
+    userNumber = userNumber * (-1);
+}
+
+for (int i = 1; i <= userNumber; i++)
+{
+    result += i;
+}
+
+System.Console.WriteLine($"Сумма чисел {userNumber} равна {result}");
