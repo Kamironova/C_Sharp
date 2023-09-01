246a440293e17959a13e1f25b693ead7f0f0db1a
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-09-01 14:52:52 +0300

    ex14

---
+++ Lec Example/Lec_Examp/Example13/Program.cs
@@ -0,0 +1,57 @@
+﻿// Создаем двумерные массивы строк и чисел
+
+Console.Clear();
+
+// Текст
+
+string[,] table = new string[2,5];
+
+//String.Empty -- инициализация пустой Строки
+
+// table[1,2] = "A";
+
+// for (int rows = 0; rows < 2; rows++) // перебирает строки
+// {
+//     for (int columns = 0; columns < 5; columns++) // перебирает столбцы
+//     {
+//         System.Console.Write($" -{table[rows, columns]}- ");
+//     }
+//  System.Console.WriteLine();
+
+// }
+
+// Числа
+
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // 0 - это строка
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // 1 - это столбец
+        {
+            System.Console.Write($"{matr[i, j]} ");
+        }
+    System.Console.WriteLine();
+}
+}
+
+void FillArray(int[,] matr)
+{for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        matr[i,j] = new Random().Next(1,10);
+    }
+}
+
+}
+
+
+
+int[,] matrix = new int[3, 4];
+
+PrintArray(matrix);
+FillArray(matrix);
+System.Console.WriteLine();
+PrintArray(matrix);
---
+++ Lec Example/Lec_Examp/Example14/Program.cs
@@ -0,0 +1,35 @@
+﻿// Закрашивание массива
+
+Console.Clear();
+
+int[,] pic = new int[,]  
+{
+    // Тут должен быть масств с картинкой из 0 и 1
+}
+
+void PrintImage(int[,] image)   // Массив который меняет 0 на пробелы и 1 на +
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            if (image[i,j] == 0) System.Console.Write(" ");
+            else System.Console.Write("+");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0 )
+    {
+        pic[row, col] = 1;
+        FillImage(row-1,col);
+        FillImage(row,col-1);
+        FillImage(row+1,col);
+        FillImage(row-1,col+1);
+    }
+}
+
+PrintImage(pic);
