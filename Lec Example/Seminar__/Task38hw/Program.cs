f2ee872a5fb1bc1c05665038a654feffd89da3e1
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-09-01 13:48:45 +0300

    hw38

---
+++ Lec Example/Seminar__/Task38hw/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+double[] GetRandomArray(int userNumber, int min, int max)
+{  
+    System.Console.Write("[ ");
+    double[] array1 = new double[userNumber];
+    for (int i = 0; i < array1.Length; i++)
+    {
+        array1[i] = new Random().Next(min, max);
+        System.Console.Write($"{array1[i]} ");
+    }
+    System.Console.Write("]");
+    System.Console.WriteLine();
+    return array1;
+}
+
+void ArrayDiff(double[] arrayD)
+{
+    double min = arrayD[0];
+    double max = arrayD[0];
+    for (int i = 0; i < arrayD.Length; i++)
+    {
+        if (arrayD[i] < min)
+        {
+            min = arrayD[i];
+        }
+        if (arrayD[i] > max)
+        {
+            max = arrayD[i];
+        }
+    }
+    System.Console.WriteLine($"{max} - {min} = {max - min}");
+}
+
+
+
+Console.Clear();
+System.Console.WriteLine("Кол-во элементов массива и его Min/Max -->");
+int userNum = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+
+double[] array = GetRandomArray(userNum, min, max);
+ArrayDiff(array);
+
