08a0ae8707627b16b9b2aeef8390969dff4cde75
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-09-13 13:52:42 +0300

    task 53

---
+++ Lec Example/Seminar__/Task55/Program.cs
@@ -0,0 +1,87 @@
+﻿// Задайте двумерный массив. 
+// Напишите программу, которая заменяет строки на столбцы.
+// В случае, если это невозможно, программа должна вывести сообщение
+// для пользователя.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// В итоге получается вот такой массив:
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
+
+
+int[,] GetRandom2DArray(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(1,10);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] array)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        System.Console.Write($"[{i}]\t");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.Write($"[{i}]\t");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i,j] +"\t");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+
+int[,] StringToRow(int[,] arr)
+{
+    int temp = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = i; j < arr.GetLength(1); j++)
+        {
+            temp = arr[i,j];
+            arr[i,j] = arr[j,i];
+            arr[j,i] = temp;
+        }
+    }
+    return arr;
+}
+
+
+
+Console.Clear();
+
+System.Console.Write("Enter row --> ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Enter column --> ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+ 
+int[,] firstArray = GetRandom2DArray(m,n);
+PrintArray(firstArray);
+if (m != n)
+{
+    System.Console.WriteLine("Преобразование не возможно!");
+}
+else
+{
+    int[,] secondArray = StringToRow(firstArray);
+    PrintArray(secondArray);
+}
