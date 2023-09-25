b78916b52350c4392394cce02181ebd978b9fb29
Карина Миронова <kamironova@192.168.1.5>
2023-09-25 22:08:01 +0300

    task69 git add Lec\ Example ww q

---
+++ Lec Example/Seminar__/Task69/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая на вход принимает два числа n и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+
+
+int ProdNum(int n, int m)
+{
+
+    if (m == 1)
+    {
+        return n;
+    }
+    else
+    {
+        return n * ProdNum(n, m-1);
+    }
+}
+
+
+
+
+Console.Clear();
+
+System.Console.Write("A --> ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("B --> ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int result = ProdNum(n, m);
+System.Console.WriteLine(result);
